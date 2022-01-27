bed1944869077df0bdc682b5dc54542cb8502a9a
DadaYaArtem <artem.morozov2004ma@gmail.com>
2022-01-27 13:30:26 +0200

    init commit

---
+++ GraphEdit/src/GetClass.java
@@ -0,0 +1,5 @@
+class getSmth{
+    public static String gettingClass(Shape ob){
+        return ob.getClass().getSimpleName();
+    }
+}
---
+++ GraphEdit/src/ShapeTest.java
@@ -0,0 +1,7 @@
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape a = new Circle("circle 1");
+        System.out.println(getSmth.gettingClass(a));
+        System.out.println(a.getName());
+    }
+}
